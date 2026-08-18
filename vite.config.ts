54c8502d4de258a5f4a8eedcc65c2aa1bd879735
Akash Kanaujiya <akashkanaujiya791@gmail.com>
2026-08-18 17:48:33 +0530

    Add @vitejs/plugin-basic-ssl for enhanced HTTPS support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/car_driving/',
+  plugins: [basicSsl()],
   server: { host: true },
   build: { target: 'es2022' },
   test: { environment: 'node' },
